1a172f1ad8964fcd0860f50866d50db0b2476d57
leerowy83-cell <leerowy83@gmail.com>
2026-04-03 12:13:16 +0300

    PROFESSIONAL AI DEV: Create PostViewController.swift for the post tab.

---
+++ InstagramClone/PostViewController.swift
@@ -0,0 +1,11 @@
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Post"
+        // Do any additional setup after loading the view.
+    }
+}
