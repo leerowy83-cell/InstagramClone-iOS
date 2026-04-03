845fa88f1fb65dcb2fc0cd3952996c064af811ef
leerowy83-cell <leerowy83@gmail.com>
2026-04-03 12:13:17 +0300

    PROFESSIONAL AI DEV: Create ProfileViewController.swift for the user profile tab.

---
+++ InstagramClone/ProfileViewController.swift
@@ -0,0 +1,11 @@
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Profile"
+        // Do any additional setup after loading the view.
+    }
+}
