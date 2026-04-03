e0ef45f0b1de3760358891c3580248cbbcc20d96
leerowy83-cell <leerowy83@gmail.com>
2026-04-03 12:13:17 +0300

    PROFESSIONAL AI DEV: Create ActivityViewController.swift for the activity/notifications tab.

---
+++ InstagramClone/ActivityViewController.swift
@@ -0,0 +1,11 @@
+
+import UIKit
+
+class ActivityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Activity"
+        // Do any additional setup after loading the view.
+    }
+}
